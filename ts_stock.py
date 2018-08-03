6cd223c3062ee72a4482e5cec93d8b87d6e84e2f
yodagast <yhuang@mail.bnu.edu.cn>
2018-08-03 15:05:31 +0800

    add data folder

--- ts_stock.py
+++ ts_stock.py
@@ -2,23 +2,56 @@ import os,sys
 import tushare as ts
 import pandas as pd
 import numpy as np
-df=ts.get_stock_basics()
-industry=pd.unique(df["industry"])
-if(os.path.exists("industry.csv")==False):
-    f=open("industry.csv","w")
-    for d in industry:
-        f.write(d + "\n")
-    f.close()
-if(os.path.exists("col.csv")==False):
-    f = open("col.csv", "w")
-    for col in df.columns:
-        f.write(col + "\n")
-    f.close()
+#report=ts.get_report_data(2018,1)
+#growth_data=ts.get_growth_data(2014,3)
+#debtpaying=ts.get_debtpaying_data(2014,3)
+#cashflow=ts.get_cashflow_data(2014,3)
 
-res=pd.DataFrame()
-for ind in industry:
-    cond=(df.industry==ind) & (df.pe>0.1) &(df.pb<3.0) & (df.pe<8.0)
-    bank=df[["name","industry","pe","pb","esp","bvps","profit"]][cond].sort_values(by="pe")
-    tmp=bank.head(20)
-    res=pd.concat([res,tmp])
-res.to_csv("res.csv",sep="\t")
+def get_basic():
+    '''
+    获取stock基本情况，pb，pe
+    :return:
+    '''
+    df = ts.get_stock_basics()
+    industry = pd.unique(df["industry"])
+    if (os.path.exists("industry.csv") == False):
+        f = open("industry.csv", "w")
+        for d in industry:
+            f.write(d + "\n")
+        f.close()
+    if (os.path.exists("col.csv") == False):
+        f = open("col.csv", "w")
+        for col in df.columns:
+            f.write(col + "\n")
+        f.close()
+    res = pd.DataFrame()
+    for ind in industry:
+        cond = (df.industry == ind) & (df.pb > 0.0) & (df.pb < 1.2) & (df.pb > 0.1) & (df.pe < 50.0)
+        bank = df[["name", "industry", "pe", "pb", "esp","npr",]][cond].sort_values(by="pe")
+        tmp = bank.head(100)
+        res = pd.concat([res, tmp])
+        res.to_csv("res.csv",sep="\t")
+    return  res
+
+def get_profit(year=2017,quarter=4):
+    file_name="./data/profit_{}_{}.tsv".format(year,quarter)
+    if (os.path.exists(file_name)):
+        profit=pd.read_csv(file_name,sep="\t",dtype={'code':'object'})
+    else:
+        profit = ts.get_profit_data(year, quarter)
+        profit.to_csv(file_name,sep="\t",index=False)
+    cond = (profit.roe>5.0)# & (profit.profits>1.0)
+    res=profit[["code","net_profit_ratio","roe"]][cond].sort_values(by="roe")
+    return res
+
+def get_growth(year=2018,quarter=1):
+    df = ts.get_growth_data(year, quarter)
+    cond = (df.mbrg > 5.0) & (df.nprg > 2.0)
+    res = df[["mbrg", "nprg", ]][cond].sort_values(by="mbrg")
+    return res
+
+basic=get_basic()
+profit=get_profit()
+res=basic.merge(profit,left_on='code',right_on='code')
+res.to_csv("basic_profit.tsv",sep="\t")
+print(res.head(50))
