0a31fa567a2a3ebb08bc711506fdc1475e539770
yodagast <yhuang@mail.bnu.edu.cn>
2019-01-15 17:23:54 +0800

    reshape xueqiu df

--- crawler/xueqiu.py
+++ crawler/xueqiu.py
@@ -25,7 +25,7 @@ def get_html(url,retry_count = 5):
     return None
 
 def get_filter_stock_columns(df):
-    columns=["code", "name",  "dividend_yield", "eps",  "pe_ttm","current", "high52w",  "low52w","limit_down", "limit_up",
+    columns=["code", "name",  "dividend_yield", "eps",  "pe_ttm","current","low52w","high52w",
              "current_year_percent","pe_forecast", "pe_lyr","pb" ,"navps", "profit", "profit_four"]
     cond =  (df.pb > 0.0) & (df.eps>0.1)&(df.dividend_yield>0.1)& (df.pe_ttm > 0.1) #& (df.pe_ttm < 30.0)
     df=df[columns][cond]
--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add .
-git commit -m "add new bash sync"
+git commit -m "rm bad features and add new features"
 git push origin master
