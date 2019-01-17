f68f83729b4986623bd7a10d1503b06bff1b65b2
yodagast <yhuang@mail.bnu.edu.cn>
2019-01-17 21:57:03 +0800

    add fund data

--- crawler/fund.py
+++ crawler/fund.py
@@ -6,13 +6,13 @@ from bs4 import BeautifulSoup
 from random import randint
 import pandas as pd
 import tushare as ts
-import sys,getopt,time,json,requests,urllib,os,platform,re
+import sys,getopt,time,json,requests,urllib,os,platform,re,copy
 
 url="http://fund.eastmoney.com/ETFN_jzzzl.html"
 
 def get_stock_info(url):
     '''
-    给定股票代码，获取雪球上的相关信息
+    给定天天基金基金数据，获取相关信息
     :param code:  example:SH601318
     :return: dict
     '''
@@ -35,18 +35,32 @@ def get_stock_info(url):
     time.sleep(randint(1,10))
     cnt=1
     print("索引\t基金代码\t基金名\t增长值\t增长率\t单位净值\t累计净值\t单位净值(昨)\t累计净值(昨)\t市价\t折价率")
+    res=[]
     for soup in BeautifulSoup(result, 'html.parser').find_all("tr",id=re.compile('^tr')):#.find_all("table", {"class": "dbtable"}):
         #print("id\tname\tgwth\tgwth_rate\tdwjz\tljjz\ty_dwjz\ty_ljjz\tprice\tcut_off")
         #print(soup.get_text(),end="\t")
-        print(cnt,end="\t")
+        #print(cnt,end="\t")
         cnt=cnt+1
-        print(soup.get("id").replace("tr",""),end="\t")
-        print(soup.find("nobr").get_text().replace("行情基金吧","").strip(),end="\t")
-        for tmp in soup.find_all("span",{"class":"zhang"}):
-            print(tmp.get_text(),end="\t")
-        for tmp in soup.find_all("span",{"class":"ping"}):
-            print(tmp.get_text(),end="\t")
-        print()
-    #return json.loads(soup)
+        tmp={}
+        #print(soup.get("id").replace("tr",""),end="\t")
+        tmp["id"]=soup.get("id").replace("tr","")
+        #print(soup.find("nobr").get_text().replace("行情基金吧","").strip(),end="\t")
+        tmp["name"]=soup.find("nobr").get_text().replace("行情基金吧","").strip()
+        ind=1
+        for t in soup.find_all("span",{"class":"zhang"}):
+            tmp["zhang_{}".format(ind)]=t.get_text()
+            ind=ind+1
+            #print(tmp.get_text(),end="\t")
+        for t in soup.find_all("span",{"class":"ping"}):
+            tmp["ping_{}".format(ind)] = t.get_text()
+            ind = ind + 1
+            #print(tmp.get_text(),end="\t")
+        print(tmp)
+        res.append(tmp)
+    #print("!!!!!!\n\t\n!!!!!")
+    return res
 
-get_stock_info(url)
+tmp=get_stock_info(url)
+res=pd.DataFrame(tmp)
+res.columns=['id', 'name', 'ping_1', 'ping_2', 'ping_3', 'ping_4', 'ping_5',
+       'ping_6', 'ping_7', 'ping_8', 'zhang_1', 'zhang_2']
