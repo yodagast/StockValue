ba0e28eb9fad990d11d23344a0e269932d2332f2
yodagast <yhuang@mail.bnu.edu.cn>
2019-06-17 18:02:51 +0800

    add pyspark example

--- crawler/pyspark-data-processor.py
+++ crawler/pyspark-data-processor.py
@@ -3,4 +3,13 @@ from pyspark.sql import SparkSession
 file = "path/README.md"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(file).cache()
+df = spark.createDataFrame([
+        (1, 144.5, 5.9, 33, 'M'),
+        (2, 167.2, 5.4, 45, 'M'),
+        (3, 124.1, 5.2, 23, 'F'),
+        (4, 144.5, 5.9, 33, 'M'),
+        (5, 133.2, 5.7, 54, 'F'),
+        (3, 124.1, 5.2, 23, 'F'),
+        (5, 129.2, 5.3, 42, 'M'),
+    ], ['id', 'weight', 'height', 'age', 'gender'])
 
