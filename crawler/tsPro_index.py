d5b5733f889f704c6affe6f2776a3fe51e6ad7ff
yodagast <yhuang@mail.bnu.edu.cn>
2019-10-17 11:23:15 +0800

    add tspro index

--- crawler/tsPro_index.py
+++ crawler/tsPro_index.py
@@ -16,10 +16,18 @@ def get_trade_vol(index_name,start_date,end_date):
 def get_trade_margin(exchange_id=None,start_date='20190901',end_date='20190910'):
     df = pro.margin(exchange_id=exchange_id,start_date=start_date,end_date=end_date)
     df["margin_amount"]=round(df["rzrqye"]/100000000,2)
+    return df.sort_values(by=["exchange_id","trade_date"])
+
+def get_trade_hsgt(start_date='20190901',end_date='20190910'):
+    df= pro.moneyflow_hsgt(start_date=start_date, end_date=end_date)
+    df["hgt"]=round(df["hgt"]/100,2)
+    df["sgt"]=round(df["sgt"]/100,2)
+    df["south_money"]=round(df["south_money"]/100,2)
     return df
+
 if __name__ == '__main__':
-    start_date='20190901'
-    end_date='20190910'
+    start_date='20191008'
+    end_date=get_recent_date()
     df=get_trade_margin(exchange_id=None,start_date=start_date,end_date=end_date)
-    print(df.head())
+    print(df.head(60))
 
